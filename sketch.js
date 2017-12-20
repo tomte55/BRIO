8c98516f67b4e7726bc60c5f668e1f749c14ff28
Fisktryne <mathiassjoberg@outlook.com>
2017-12-20 15:20:28 +0100

    AUTO BOTS ROLL OUT

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+var x = 0;
+var y = 0;
+var r = 50;
+
+var printed = false;
 
 function setup() {
 	createCanvas(500, 500);
@@ -5,5 +10,14 @@ function setup() {
 
 function draw() {
 	background(150);
-	
+
+	// Ritar en Rektangel i mitten av skärmen
+	ellipse(x, y, r,);
+
+}
+
+
+function mousePressed() {
+	x = mouseX;
+	y = mouseY;
 }

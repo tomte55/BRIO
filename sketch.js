9584a70a99e2463eb8a55a23d9b7c860c870b946
tomte55 <tomte55@hotmail.se>
2017-12-20 16:15:27 +0100

    Cleaned sketch

--- sketch.js
+++ sketch.js
@@ -1,22 +1,9 @@
-var x = 0;
-var y = 0;
-var r = 50;
 
 function setup() {
 	createCanvas(500, 500);
-	rectMode(CENTER);
 }
 
 function draw() {
 	background(150);
 
-	// Ritar en Rektangel i mitten av skärmen
-	ellipse(x, y, r,);
-
-}
-
-
-function mousePressed() {
-	x = mouseX;
-	y = mouseY;
 }

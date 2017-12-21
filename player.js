535cd762c62e96d5aeaf8140b9779e1c9d14cf20
tomte55 <tomte55@hotmail.se>
2017-12-21 18:34:10 +0100

    Shit in ass

--- player.js
+++ player.js
@@ -1,11 +1,8 @@
 function Player() {
-  this.x = 250;
-  this.y = 250;
-  this.xs = 50;
-  this.ys = 50;
+  this.pos = createVector(250, 250);
 
 
   this.show = function() {
-    rect(this.x, this.y, this.xs, this.ys);
+
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 function setup() {
 	createCanvas(500, 500);
+	player = new Player();
+	rectMode(CENTER);
 }
 
 function draw() {
 	background(150);
 
-
+	player.show();
+	player.update();
 }
